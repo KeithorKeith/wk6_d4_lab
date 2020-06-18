6492d815811b14982e7510917fb95d3d5495e7b4
Craig McLeod <mcleod.craig@outlook.com>
2020-06-18 15:01:51 +0100

    Console logs all working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  const button = document.querySelector('input');
+  button.addEventListener('click', handleButtonClick);
+
+  const textInput = document.querySelector('#title');
+  textInput.addEventListener('input', handleInput);
+
+  const select = document.querySelector('#category')
+  select.addEventListener('change', handleSelect);
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+const handleButtonClick = function () {
+console.log('Button clickin');
+}
+
+const handleInput = function () {
+console.log('Inputtin!');
+}
+
+const handleSelect = function () {
+  console.log('Selectin!');
+}
+
+const handleFormSubmit = function (event) {
+  event.preventDefault()
+  console.log('submittin!');
+  
+}
