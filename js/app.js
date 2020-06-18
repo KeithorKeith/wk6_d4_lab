9aac2d687d4819437d559cea3ac55b6aa3f8301a
Craig McLeod <mcleod.craig@outlook.com>
2020-06-18 15:44:45 +0100

    MVP finished

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const button = document.querySelector('input');
   button.addEventListener('click', handleButtonClick);
 
-  const textInput = document.querySelector('#title');
-  textInput.addEventListener('input', handleInput);
+  const textInput1 = document.querySelector('#title');
+  textInput1.addEventListener('input', handleInput);
+
+  const textInput2 = document.querySelector('#author');
+  textInput2.addEventListener('input', handleInput)
 
   const select = document.querySelector('#category')
   select.addEventListener('change', handleSelect);
@@ -25,7 +28,19 @@ const handleSelect = function () {
 }
 
 const handleFormSubmit = function (event) {
+  const textInput1 = document.querySelector('#title').value;
+  const textInput2 = document.querySelector('#author').value;
+  const select = document.querySelector('#category').value;
   event.preventDefault()
-  console.log('submittin!');
-  
+
+  console.log(textInput1);
+  console.log(textInput2);
+  console.log(select);
+
+  const reading_list = document.querySelector('#reading-list');
+  const listItem = document.createElement('li')
+  listItem.textContent = `${textInput1} - ${textInput2}... ${select}`;
+  reading_list.appendChild(listItem)
+
+  // this.reset();
 }
